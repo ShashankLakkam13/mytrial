32854665a47356636cbb19321aa9f15789723065
shashank Reddy <shashank970613@gmail.com>
2023-08-28 12:43:34 -0400

    initial commit

---
+++ src/test/java/in/shashank/AppTest.java
@@ -0,0 +1,20 @@
+package in.shashank;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
